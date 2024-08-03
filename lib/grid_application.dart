844797abd95a6cdca99658538f5c4db8d50d1460
dhara58 <dharalunagariya5898@gmail.com>
2024-08-03 18:37:08 +0530

    first commit

--- lib/grid_application.dart
+++ lib/grid_application.dart
@@ -11,206 +11,226 @@ class _GridApplicationState extends State<GridApplication> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.lightBlueAccent,
-        title: Column(
+        appBar: AppBar(
+          backgroundColor: Colors.lightBlueAccent,
+          title: Column(
+            children: [
+              Row(
+                children: [
+                  Text('Flutter Grid Dashboard'),
+                ],
+              ),
+              Row(
+                children: [
+                  Text('Items'),
+                ],
+              ),
+            ],
+          ),
+        ),
+        body: Column(
           children: [
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Text('Flutter Grid Dashboard'),
+                Container(
+                  height: 180,
+                  width: 180,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadiusDirectional.circular(5),
+                    color: Colors.blue,
+                  ),
+                  child: Center(
+                      child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        children: [
+                          Icon(
+                            Icons.calendar_month,
+                            size: 55,
+                          ),
+                        ],
+                      ),
+                      Center(
+                        child: Center(
+                          child: Text(
+                            'Calender',
+                            style: TextStyle(fontSize: 30),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )),
+                ),
+                Container(
+                  height: 180,
+                  width: 180,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadiusDirectional.circular(5),
+                    color: Color.fromARGB(255, 255, 215, 179),
+                  ),
+                  child: Center(
+                      child: Column(
+                    children: [
+                      Icon(
+                        Icons.festival,
+                        size: 55,
+                      ),
+                      Center(
+                        child: Center(
+                          child: Text(
+                            'Festival',
+                            style: TextStyle(fontSize: 30),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )),
+                ),
               ],
             ),
-            
             Row(
               children: [
-                Text('Items'),
+                Container(
+                  child: Center(
+                      child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(25, 25, 25, 25),
+                        child: Icon(
+                          Icons.food_bank,
+                          size: 50,
+                        ),
+                      ),
+                      Center(
+                        child: Center(
+                          child: Padding(
+                            padding: const EdgeInsets.only(),
+                            child: Text(
+                              'Food',
+                              style: TextStyle(fontSize: 30),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )),
+                  height: 180,
+                  width: 180,
+                  margin: EdgeInsets.all(10),
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadiusDirectional.circular(5),
+                    color: Color.fromARGB(255, 253, 199, 255),
+                  ),
+                ),
+                Container(
+                  child: Center(
+                      child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(25, 25, 25, 25),
+                        child: Icon(
+                          Icons.location_city,
+                          size: 50,
+                        ),
+                      ),
+                      Center(
+                        child: Center(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'Location',
+                              style: TextStyle(fontSize: 30),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )),
+                  height: 180,
+                  width: 180,
+                  margin: EdgeInsets.all(10),
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadiusDirectional.circular(5),
+                    color: Color.fromARGB(255, 196, 255, 252),
+                  ),
+                ),
               ],
             ),
-            
+            Row(
+              children: [
+                Container(
+                  child: Center(
+                      child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(25, 25, 25, 25),
+                        child: Icon(
+                          Icons.today_outlined,
+                          size: 50,
+                        ),
+                      ),
+                      Center(
+                        child: Center(
+                          child: Padding(
+                            padding: const EdgeInsets.only(),
+                            child: Text(
+                              'Todo',
+                              style: TextStyle(fontSize: 30),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )),
+                  height: 180,
+                  width: 180,
+                  margin: EdgeInsets.all(10),
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadiusDirectional.circular(5),
+                    color: Color.fromARGB(255, 255, 171, 175),
+                  ),
+                ),
+                Container(
+                  child: Center(
+                      child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(25, 25, 25, 25),
+                        child: Icon(
+                          Icons.settings,
+                          size: 50,
+                        ),
+                      ),
+                      Center(
+                        child: Center(
+                          child: Padding(
+                            padding: const EdgeInsets.only(),
+                            child: Text(
+                              'Settings',
+                              style: TextStyle(fontSize: 30),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )),
+                  height: 180,
+                  width: 180,
+                  margin: EdgeInsets.all(10),
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadiusDirectional.circular(5),
+                    color: Color.fromARGB(255, 255, 178, 214),
+                  ),
+                ),
+              ],
+            ),
           ],
-        ),
-                     
-      ),
-       body: Column(
-        children: [
-          Row(
-            children: [
-              
-              Container(
-                child: Center(child: Column(
-                  children: [
-                    Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(25, 25, 25, 25),
-                          child: Icon(Icons.calendar_month,size: 55,),
-                        ),
-                      ],
-                    ),
-                    Center(
-                      child: Center(
-                        child: Padding(
-                          padding: const EdgeInsets.only(),
-                          child: Text('Calender',style: TextStyle(fontSize: 30),),
-                        ),
-                      ),
-                    ),
-                                       
-                  ],
-                )),
-                height: 180,  
-                width: 180,
-                margin: EdgeInsets.all(10),
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadiusDirectional.circular(5),
-                  color: Colors.blue,
-                ),
-                
-              ),
-              Container(
-                child: Center(child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(25, 25, 25, 25),
-                      child: Icon(Icons.festival,size: 55,),
-                    ),
-                    Center(
-                      child: Center(
-                        child: Padding(
-                          padding: const EdgeInsets.only(),
-                          child: Text('Festival',style: TextStyle(fontSize: 30),),
-                        ),
-                      ),
-                    ),
-                  ],
-                )),
-                height: 180,
-                width: 180,
-                margin: EdgeInsets.all(10),
-                padding: EdgeInsets.all(10),   
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadiusDirectional.circular(5),
-                  color: Color.fromARGB(255, 255, 215, 179),
-                ),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Container(
-                child: Center(child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(25, 25, 25, 25),
-                      child: Icon(Icons.food_bank,size: 50,),
-                    ),
-                    Center(
-                      child: Center(
-                        child: Padding(
-                          padding: const EdgeInsets.only(),
-                          child: Text('Food',style: TextStyle(fontSize: 30),),
-                        ),
-                      ),
-                    ),
-                  ],
-                )),
-                height: 180,
-                width: 180,
-                margin: EdgeInsets.all(10),
-                padding: EdgeInsets.all(10),   
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadiusDirectional.circular(5),
-                  color: Color.fromARGB(255, 253, 199, 255),
-                ),
-              ),
-              Container(
-                child: Center(child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(25, 25, 25, 25),
-                      child: Icon(Icons.location_city,size: 50,),
-                    ),
-                    Center(
-                      child: Center(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text('Location',style: TextStyle(fontSize: 30),),
-                        ),
-                      ),
-                    ),
-                  ],
-                )),
-                height: 180,
-                width: 180,
-                margin: EdgeInsets.all(10),
-                padding: EdgeInsets.all(10),   
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadiusDirectional.circular(5),
-                  color: Color.fromARGB(255, 196, 255, 252),
-                ),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Container(
-                child: Center(child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(25, 25, 25, 25),
-                      child: Icon(Icons.today_outlined,size: 50,),
-                    ),
-                    Center(
-                      child: Center(
-                        child: Padding(
-                          padding: const EdgeInsets.only(),
-                          child: Text('Todo',style: TextStyle(fontSize: 30),),
-                        ),
-                      ),
-                    ),
-                  ],
-                )),
-                height: 180,
-                width: 180,
-                margin: EdgeInsets.all(10),
-                padding: EdgeInsets.all(10),   
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadiusDirectional.circular(5),
-                  color: Color.fromARGB(255, 255, 171, 175),
-                ),
-              ),
-              Container(
-                child: Center(child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(25, 25, 25, 25),
-                      child: Icon(Icons.settings,size: 50,),
-                    ),
-                    Center(
-                      child: Center(
-                        child: Padding(
-                          padding: const EdgeInsets.only(),
-                          child: Text('Settings',style: TextStyle(fontSize: 30),),
-                        ),
-                      ),
-                    ),
-                  ],
-                )),
-                height: 180,
-                width: 180,
-                margin: EdgeInsets.all(10),
-                padding: EdgeInsets.all(10),   
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadiusDirectional.circular(5),
-                  color: Color.fromARGB(255, 255, 178, 214),
-                ),
-              ),
-            ],
-          ),
-
-        ],
-       )
-    );
+        ));
   }
 }
